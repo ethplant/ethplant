c4365a5dd23f84421f26d3a43c3ac2a8f77e0913
Kartik, Chen <kbehouse@gmail.com>
2020-11-03 10:47:30 +0800

    feat: 🎸 init

---
+++ precache-manifest.4b2fe9152fd25e8bbdb69049fcd8a7e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590d498f20b056f83352ed509aa25fa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67bd53007ee27a0493ad",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4583ba300fb03c83249c",
+    "url": "/static/css/main.e07c89f3.chunk.css"
+  },
+  {
+    "revision": "67bd53007ee27a0493ad",
+    "url": "/static/js/2.de0b6f63.chunk.js"
+  },
+  {
+    "revision": "9719e4e1322c7352c9449ffa3c367e70",
+    "url": "/static/js/2.de0b6f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4583ba300fb03c83249c",
+    "url": "/static/js/main.2047b0b6.chunk.js"
+  },
+  {
+    "revision": "5caf021399d84750a193",
+    "url": "/static/js/runtime-main.d3fcc571.js"
+  }
+]);
