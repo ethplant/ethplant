20e67f629898a411d6b63ca82092d32671b649af
Kartik, Chen <kbehouse@gmail.com>
2020-11-17 00:05:57 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.907dc93521da40307c44cfb6cc375e3a.js"
+  "/precache-manifest.752e1db5fb3123b0acbef29a40c99c7c.js"
 );
 
 self.addEventListener('message', (event) => {
